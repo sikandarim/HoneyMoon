ab948aca3e136ad3c5802e4ba356ea98309088ac
mariam <mariamsikandari22@example.com>
2023-11-13 16:08:29 -0500

    Initial Commit

---
+++ HoneyMoon/HoneyMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyMoonApp.swift
+//  HoneyMoon
+//
+//  Created by Mariam Sikandari on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct HoneyMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
